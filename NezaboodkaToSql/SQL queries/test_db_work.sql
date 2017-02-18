9df8288ee1690bd5d08f2067afa06648fe3efe96
Vladislav Savionok <savionak@gmail.com>
2017-02-19 00:31:41 +0300

    Update installation script
    + update test_db_work script

--- NezaboodkaToSql/SQL queries/test_db_work.sql
+++ NezaboodkaToSql/SQL queries/test_db_work.sql
@@ -5,56 +5,51 @@
         
 **********************************************/
 
-use `nz_admin_db`;
+USE `nz_admin_db`;
 
 CALL before_alter_database_list();
 
-insert into `db_rem_list`
-values ('nz_test_db');
+INSERT INTO `db_rem_list`
+VALUES ('nz_test_db');
 
-insert into `db_add_list`
-values ('nz_test_db');
+INSERT INTO `db_add_list`
+VALUES ('nz_test_db');
 
 CALL alter_database_list();
 CALL cleanup_removed_databases();
 
 
-/*  Fill test database schema */
+/*  Alter test database schema */
 
-USE `nz_test_db`;
-
-INSERT INTO `type`
+CALL before_alter_database_schema();
+INSERT INTO `type_add_list`
 (`name`, `table_name`, `base_type_name`)
 VALUES
 ('User', '_user', ''),
 ('Admin', '_admin', 'User'),
 ('Group', '_group', '');
 
-
-INSERT INTO `field`
+INSERT INTO `field_add_list`
 (`name`, `col_name`, `owner_type_name`, `type_name`, `is_list`, `compare_options`, `back_ref_name`)
 VALUES
 # User
-('Login', '_login', 'User', 'VARCHAR(60)', FALSE, 'IgnoreCase', NULL),
-('Email', '_email', 'User', 'VARCHAR(120)', FALSE, 'IgnoreCase', NULL),
-('PassHash', '_pass_hash', 'User', 'VARCHAR(128)', FALSE, 'None', NULL),
+('Login', '_login', 'User', 'VARCHAR(60)', FALSE, 'IgnoreCase', ''),
+('Email', '_email', 'User', 'VARCHAR(120)', FALSE, 'IgnoreCase', ''),
+('PassHash', '_pass_hash', 'User', 'VARCHAR(128)', FALSE, 'None', ''),
 ('Group', '_group', 'User', 'Group', FALSE, 'None', 'Participants'),
-('Age', '_age', 'User', 'INT UNSIGNED?', FALSE, 'None', NULL),
-
+('Age', '_age', 'User', 'INT UNSIGNED?', FALSE, 'None', ''),
 #Admin
 ('ControlGroup', '_control_group', 'Admin', 'Group', FALSE, 'None', 'Admin'),
-
 #Group
-('Title', '_title', 'Group', 'VARCHAR(255)', FALSE, 'None', NULL),
+('Title', '_title', 'Group', 'VARCHAR(255)', FALSE, 'None', ''),
 ('Admin', '_admin', 'Group', 'Admin', FALSE, 'None', 'ControlGroup'),
 ('Participants', '_participants', 'Group', 'User', TRUE, 'None', 'Group'),
-('Rating', '_rating', 'Group', 'INT UNSIGNED', FALSE, 'None', NULL),
-('DescriptionText', '_description_text', 'Group', 'TEXT', FALSE, 'IgnoreCase', NULL);
+('Rating', '_rating', 'Group', 'INT UNSIGNED', FALSE, 'None', ''),
+('DescriptionText', '_description_text', 'Group', 'TEXT', FALSE, 'IgnoreCase', '');
 
+CALL alter_database_schema('nz_test_db');
 
-CALL `nz_admin_db`.alter_db_schema('nz_test_db');
-
-#  *** Get nz_test_db schema ***
+-- Get nz_test_db schema
 
 USE `nz_test_db`;
 
