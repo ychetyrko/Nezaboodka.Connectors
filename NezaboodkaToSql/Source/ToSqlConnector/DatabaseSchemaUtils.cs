716ae722d8c47cb0fa909b8f6a06f5e9e85cda3f
Vladislav Savionok <savionak@gmail.com>
2017-03-05 01:06:40 +0300

    Update DatabaseSchemaUtils
    * implement GetUpdateTypesFields

--- NezaboodkaToSql/Source/ToSqlConnector/DatabaseSchemaUtils.cs
+++ NezaboodkaToSql/Source/ToSqlConnector/DatabaseSchemaUtils.cs
@@ -14,16 +14,106 @@ namespace Nezaboodka.ToSqlConnector
             ClientTypeSystem oldTypeSystem = new ClientTypeSystem(oldDatabaseSchema.TypeDefinitions);
             ClientTypeSystem newTypeSystem = new ClientTypeSystem(newDatabaseSchema.TypeDefinitions);
 
-            List<string> newTypes, newFields;
-            GetNewTypesFields(oldTypeSystem, newTypeSystem, out newTypes, out newFields);
+            List<string> typesToAdd, fieldsToAdd;
+            List<string> typesToRemove, fieldsToRemove;
 
-            result.TypesToAdd.AddRange(newTypes);
-            result.FieldsToAdd.AddRange(newFields);
+            GetUpdatedTypesFields(oldTypeSystem, newTypeSystem, out typesToAdd, out fieldsToAdd, out typesToRemove,
+                out fieldsToRemove);
+            result.TypesToAdd.AddRange(typesToAdd);
+            result.FieldsToAdd.AddRange(fieldsToAdd);
+            result.TypesToRemove.AddRange(typesToRemove);
+            result.FieldsToRemove.AddRange(fieldsToRemove);
+
+            GetNewTypesFields(oldTypeSystem, newTypeSystem, out typesToAdd, out fieldsToAdd);
+            result.TypesToAdd.AddRange(typesToAdd);
+            result.FieldsToAdd.AddRange(fieldsToAdd);
 
             return result;
         }
 
         // Internal
+
+        private static void GetUpdatedTypesFields(ClientTypeSystem oldTypeSystem, ClientTypeSystem newTypeSystem,
+            out List<string> typesToAdd, out List<string> fieldsToAdd,
+            out List<string> typesToRemove, out List<string> fieldsToRemove)
+        {
+            typesToAdd = new List<string>();
+            fieldsToAdd = new List<string>();
+            typesToRemove = new List<string>();
+            fieldsToRemove = new List<string>();
+
+            for (int oldTypeNumber = 0; oldTypeNumber < oldTypeSystem.GetTypeCount(); ++oldTypeNumber)
+            {
+                string typeName = oldTypeSystem.GetTypeName(oldTypeNumber);
+                int typeNumber = newTypeSystem.GetTypeNumberByName(typeName);
+                if (typeNumber >= 0)
+                {
+                    if (oldTypeSystem.TypeDefinitions[oldTypeNumber].BaseTypeName ==
+                        newTypeSystem.TypeDefinitions[typeNumber].BaseTypeName)
+                    {
+                        int fieldCount = oldTypeSystem.GetFieldCount(oldTypeNumber);
+                        for (int oldFieldNumber = 0; oldFieldNumber < fieldCount; ++oldFieldNumber)
+                        {
+                            int fieldNumber = newTypeSystem.GetFieldNumberByName(typeNumber,
+                                oldTypeSystem.GetFieldName(oldTypeNumber, oldFieldNumber));
+                            if (fieldNumber >= 0)
+                            {
+                                if (oldTypeSystem.GetFieldTypeName(oldTypeNumber, oldFieldNumber) ==
+                                    newTypeSystem.GetFieldTypeName(typeNumber,
+                                        fieldNumber))
+                                {
+                                    // TODO: update BackReferences if needed
+                                }
+                                else
+                                {
+                                    FieldDefinition fieldDefinition = newTypeSystem.GetFieldDefinition(typeNumber,
+                                        fieldNumber);
+                                    string fieldAddString = GetAddFieldString(typeName, fieldDefinition);
+                                    fieldsToAdd.Add(fieldAddString);
+                                    fieldNumber = -1;
+                                }
+                            }
+                            if (fieldNumber == -1)
+                            {
+                                FieldDefinition fieldDefinition = oldTypeSystem.GetFieldDefinition(oldTypeNumber,
+                                    oldFieldNumber);
+                                string fieldRemoveString = GetRemoveFieldString(typeName, fieldDefinition);
+                                fieldsToRemove.Add(fieldRemoveString);
+                            }
+                        }
+
+                        for (int newFieldNumber = 0;
+                            newFieldNumber < newTypeSystem.GetFieldCount(oldTypeNumber);
+                            ++newFieldNumber)
+                        {
+                            int fieldNumber = oldTypeSystem.GetFieldNumberByName(oldTypeNumber,
+                                newTypeSystem.GetFieldName(typeNumber, newFieldNumber));
+                            if (fieldNumber == -1)
+                            {
+                                FieldDefinition fieldDefinition = newTypeSystem.GetFieldDefinition(typeNumber,
+                                    fieldNumber);
+                                string fieldAddString = GetAddFieldString(typeName, fieldDefinition);
+                                fieldsToAdd.Add(fieldAddString);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        TypeDefinition typeDefinition = newTypeSystem.TypeDefinitions[typeNumber];
+                        string typeAddString = GetAddTypeString(typeDefinition);
+                        typesToAdd.Add(typeAddString);
+                        AddAllTypeFields(fieldsToAdd, typeDefinition);
+                        typeNumber = -1;
+                    }
+                }
+                if (typeNumber == -1)
+                {
+                    TypeDefinition typeDefinition = oldTypeSystem.TypeDefinitions[oldTypeNumber];
+                    string typeRemoveString = GetRemoveTypeString(typeDefinition);
+                    typesToRemove.Add(typeRemoveString);
+                }
+            }
+        }
 
         private static void GetNewTypesFields(ClientTypeSystem oldTypeSystem, ClientTypeSystem newTypeSystem,
             out List<string> newTypes, out List<string> newFields)
@@ -39,13 +129,17 @@ namespace Nezaboodka.ToSqlConnector
                     TypeDefinition typeDefinition = newTypeSystem.TypeDefinitions[i];
                     string typeAddString = GetAddTypeString(typeDefinition);
                     newTypes.Add(typeAddString);
-
-                    foreach (var fieldDefinition in typeDefinition.FieldDefinitions)
-                    {
-                        string fieldAddString = GetAddFieldString(typeDefinition.TypeName, fieldDefinition);
-                        newFields.Add(fieldAddString);
-                    }
+                    AddAllTypeFields(newFields, typeDefinition);
                 }
+            }
+        }
+
+        private static void AddAllTypeFields(List<string> fieldsList, TypeDefinition typeDefinition)
+        {
+            foreach (var fieldDefinition in typeDefinition.FieldDefinitions)
+            {
+                string fieldAddString = GetAddFieldString(typeDefinition.TypeName, fieldDefinition);
+                fieldsList.Add(fieldAddString);
             }
         }
 
